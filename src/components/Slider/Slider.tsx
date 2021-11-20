42bee5aa72f8f593b1a94ad8e9c9e3c6b767906f
Maxim <4andromed4@gmail.com>
2021-11-20 16:06:37 +0100

    started double_slider implementation

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -6,23 +6,30 @@ interface SliderProps {
   step: number;
   min: number;
   max: number;
-  value: number;
+  firstValue: number;
+  secondValue: number;
   unit: string;
   range?: boolean;
 }
 
 export const Slider = ({ ...props }: SliderProps, ) => {
 
-  const [inputValue, setInputValue] = useState(props.value);
-  useEffect(() => {setInputValue(props.value)}, [props.value])
+  const [inputValue1, setInputValue1] = useState(props.firstValue);
+  const [inputValue2, setInputValue2] = useState(props.secondValue);
+  useEffect(() => {setInputValue1(props.firstValue)}, [props.firstValue])
+  useEffect(() => {setInputValue2(props.secondValue)}, [props.secondValue])
 
   props.unit = "Unit"
 
+  var input2 = <input className="Textfield Textfield-second" value={inputValue2} onChange={(event) => {setInputValue2(+event.target.value)}}></input>
+
   return <div>
-          <input className="Textfield" value={inputValue} onChange={(event) => {setInputValue(+event.target.value)}}></input> 
+          <input className="Textfield" value={inputValue1} onChange={(event) => {setInputValue1(+event.target.value)}}></input> 
+          {props.range ? <span className="Textfield-dash">-</span> : null}
+          {props.range ? input2 : null}
           <span className="Unitlabel">{props.unit}</span>
           <input type="range" className="Slider" 
-                  onChange={(event) => {setInputValue(+event.target.value);}} 
-                  step={props.step} min={props.min} max={props.max} value={inputValue}></input>
+                  onChange={(event) => {setInputValue1(+event.target.value);}} 
+                  step={props.step} min={props.min} max={props.max} value={inputValue1}></input>
         </div>;
 };
