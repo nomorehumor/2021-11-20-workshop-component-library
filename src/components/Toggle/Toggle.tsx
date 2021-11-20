b2a39f2a0da44d2ad06aa33381a13c79ba557a0c
Jonathan Ehwald <github@ehwald.info>
2021-11-20 14:25:46 +0100

    Add Toggle component

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Toggle.css";
 
-interface TextareaProps {}
+interface TextareaProps {
+  label: string;
+}
 
-export const Toggle = ({ ...props }: TextareaProps) => {
-  // --- YOUR CODE HERE ---
-
-  return <>Toggle</>;
+export const Toggle = ({ label, ...props }: TextareaProps) => {
+  return (
+    <label className="toggle">
+      <input className="toggle-input" type="checkbox" />
+      <div className="toggle-box">
+        <div className="toggle-ball"></div>
+      </div>
+      <span className="toggle-label">{label}</span>
+    </label>
+  )
 };
